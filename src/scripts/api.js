36f201ad87976def0c29d0289efa4530087a506d
totiard <165372513+totiard@users.noreply.github.com>
2024-12-09 01:08:11 +0700

    Update api.js

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,5 +1,5 @@
 // TODO: Silakan sesuaikan BASE URL dari endpoint Anda
-const BASE_URL = 'https://cancerpredict-180338769224.asia-southeast2.run.app/';
+const BASE_URL = 'https://cancerpredict-180338769224.asia-southeast2.run.app/'; //ganti dengan url backend kalian, atau cari di repo saya dengan nama (asclepius-backend)
 
 const ENDPOINT = {
   predict: `${BASE_URL}/predict`,
